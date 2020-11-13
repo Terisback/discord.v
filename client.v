2c6d42245c891d1fe36efa8be85ac42b4e671647
Anton Terisback <terisbackno@gmail.com>
2020-11-13 17:19:04 +0500

    🌌 Implement shards (not working, at all)

--- client.v
+++ client.v
@@ -1,63 +1,66 @@
 module discordv
 
-import time
-import x.websocket
+import sync
+import eventbus
 
-const (
-	default_gateway = 'wss://gateway.discord.gg:443/?v=8&encoding=json'
-)
+pub struct Config {
+pub mut:
+	token string
+	shard_count int
+	intents Intent = intents.guilds | intents.guild_messages
+}
 
 pub struct Client {
 	token string
 	intents Intent
 mut:
-	events map[string][]EventHandler
-	ws &websocket.Client
-	session_id string
-	sequence int
-	heartbeat_interval u64
-	last_heartbeat u64
+	wg &sync.WaitGroup
+	events &eventbus.EventBus
+	shards []&Shard
 }
 
-pub fn new(token string) ?&Client{
-	mut ws := websocket.new_client(default_gateway) or {
-		return error('[discord.v] Unavailable to instantiate websocket client')
+pub fn new(config Config) ?&Client{
+	mut c := &Client{
+		token: config.token
+		intents: config.intents
+		wg: sync.new_waitgroup()
+		events: eventbus.new()
 	}
 
-	mut c := &Client{
-		token: token,
-		events: map[string][]EventHandler{}
-		ws: ws
-		intents: 0
-	}	
+	mut cfg := config
+	if cfg.shard_count == 0 {
+		cfg.shard_count = c.get_shard_count()?
+	}
 
-	c.ws.on_open_ref(ws_on_open, c)
-	c.ws.on_error_ref(ws_on_error, c)
-	c.ws.on_message_ref(ws_on_message, c)
-	c.ws.on_close_ref(ws_on_close, c)
+	mut shards := []&Shard{}
+	for i in 0..cfg.shard_count {
+		mut shard := new_shard(mut c, i)?
+		shards << shard
+	}
+	c.shards = shards
+
+	c.wg.add(cfg.shard_count)
 
 	return c
 }
 
-pub fn (mut c Client) on(event Event, handler EventHandler){
-	c.events[event.str()] << handler
+pub fn (mut c Client) on(event Event, handler eventbus.EventHandlerFn){
+	c.events.subscriber.subscribe(event.str(), handler)
 }
 
 pub fn (mut c Client) stay_connected() ? {
-	println('[discord.v] Connecting to gateway...')
-	c.ws.connect()?
-	go c.ws.listen()?
-	for true {
-		time.sleep(1)
-		if time.now().unix_time_milli() - c.last_heartbeat > c.heartbeat_interval {
-			heartbeat := HeartbeatPacket {
-				op: Op.heartbeat,
-				data: c.sequence
-			}
-			println("[discord.v] Heartbeat $c.sequence")
-			message := heartbeat.to_json()
-			c.ws.write(message.bytes(), .text_frame)
-			c.last_heartbeat = time.now().unix_time_milli()
-		}
+	for i in 0..c.shards.len{
+		c.shards[i].start()?
+	}
+	c.wg.wait()
+}
+
+pub fn (mut c Client) run() ? {
+	go c.stay_connected()?
+}
+
+pub fn (mut c Client) stop() {
+	for i in 0..c.shards.len{
+		c.shards[i].stop <- true
 	}
 }
--- events.v
+++ events.v
@@ -3,11 +3,43 @@ module discordv
 pub type EventHandler = fn(mut c Client, event voidptr)
 
 pub enum Event {
+	hello
 	ready
+	resumed
+	reconnect
+	invalid_session
+	channel_create
+	channel_update
+	channel_delete
+	channel_pins_update
+	guild_create
+	guild_update
+	guild_delete
+	guild_ban_add
+	guild_ban_remove
+	guild_emojis_update
+	guild_integrations_update
+	guild_member_add
+	guild_member_remove
+	guild_member_update
+	guild_members_chunk
+	guild_role_create
+	guild_role_update
+	guild_role_delete
+	invite_create
+	invite_delete
+	message_create
+	message_update
+	message_delete
+	message_delete_bulk
+	message_reaction_add
+	message_reaction_remove
+	message_reaction_remove_all
+	message_reaction_remove_emoji
+	presence_update
+	typing_start
+	user_update
+	voice_state_update
+	voice_server_update
+	webhooks_update
 }
-
-const (
-	event_list = {
-		"ready": Ready{},
-	}
-)
