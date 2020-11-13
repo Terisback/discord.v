37c1b5713c2991f8c17f383362bc4e68c0df42a6
Anton Terisback <terisbackno@gmail.com>
2020-11-13 17:16:25 +0500

    🖍️ Cool colorful log function

---
+++ log.v
@@ -0,0 +1,19 @@
+module discordv
+
+import term
+
+// Remove then release
+
+const (
+	colors_supported = term.can_show_color_on_stdout()	
+)
+
+fn log(txt string) {
+	match colors_supported {
+		true { 
+			v := term.rgb(95, 155, 230, 'v')
+			println(term.bright_white('[discord.$v] ') + txt) 
+		}
+		else { println('[discord.v] ' + txt) }
+	}
+}
