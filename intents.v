64fa18206ba7121e4c899c84db7a47645e11e737
Anton Terisback <terisbackno@gmail.com>
2020-11-13 17:14:49 +0500

    📜 Implement intents

--- intents.v
+++ intents.v
@@ -1,21 +1,28 @@
 module discordv
 
-pub type Intent = i64
+pub type Intent = u16
+
+pub struct Intents {
+pub:
+	guilds Intent = Intent(1 << 0)
+	guild_members Intent = Intent(1 << 1)
+	guild_bans Intent = Intent(1 << 2)
+	guild_emojis Intent = Intent(1 << 3)
+	guild_integrations Intent = Intent(1 << 4)
+	guild_webhooks Intent = Intent(1 << 5)
+	guild_invites Intent = Intent(1 << 6)
+	guild_voice_states Intent = Intent(1 << 7)
+	guild_precenses Intent = Intent(1 << 8)
+	guild_messages Intent = Intent(1 << 9)
+	guild_message_reactions Intent = Intent(1 << 10)
+	guild_message_typing Intent = Intent(1 << 11)
+	direct_messages Intent = Intent(1 << 12)
+	direct_message_reactions Intent = Intent(1 << 13)
+	direct_message_typing Intent = Intent(1 << 14)
+	all_allowed Intent = Intent(32509)
+	all Intent = Intent(32767)
+}
 
 pub const (
-	intent_guilds = Intent(1 << 0)
-	intent_guild_members = Intent(1 << 1)
-	intent_guild_bans = Intent(1 << 2)
-	intent_guild_emojis = Intent(1 << 3)
-	intent_guild_integrations = Intent(1 << 4)
-	intent_guild_webhooks = Intent(1 << 5)
-	intent_guild_invites = Intent(1 << 6)
-	intent_guild_voice_states = Intent(1 << 7)
-	intent_guild_precenses = Intent(1 << 8)
-	intent_guild_messages = Intent(1 << 9)
-	intent_guild_message_reactions = Intent(1 << 10)
-	intent_guild_message_typing = Intent(1 << 11)
-	intent_direct_messages = Intent(1 << 12)
-	intent_direct_message_reactions = Intent(1 << 13)
-	intent_direct_message_typing = Intent(1 << 14)
+	intents = Intents{}
 )
