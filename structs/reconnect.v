30098a5bbd38ecaedff2c73f4f547642fd42063c
Anton Terisback <terisbackno@gmail.com>
2020-11-23 09:58:38 +0000

    🕦 Implement reconnect from_json method

--- structs/reconnect.v
+++ structs/reconnect.v
@@ -1,6 +1,15 @@
 module structs
 
+import x.json2 as json
+
 pub struct Reconnect {
-pub:
+pub mut:
 	resumed bool
+}
+
+pub fn (mut r Reconnect) from_json(f json.Any) {
+	mut obj := f.as_map()
+	if 'resumed' in obj{
+		r.resumed = obj['resumed'].bool()
+	}
 }
