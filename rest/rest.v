4652ca2d9cc7448729a713d861169f3a2dcc77c1
div72 <60045611+div72@users.noreply.github.com>
2020-11-28 21:42:32 +0300

    rest: add ratelimits

--- rest/rest.v
+++ rest/rest.v
@@ -1,6 +1,9 @@
 module rest
 
+import discordv.util
 import net.http
+import sync
+import time
 
 pub struct REST {
 pub:
@@ -17,7 +20,33 @@ pub fn new(token string) &REST{
 }
 
 pub fn (mut rest REST) do(req http.Request) ?http.Response {
-	// TODO: RateLimit thingy there
+	bucket := req.url
+	mut mlock := rest.rl.m_lock(bucket)
 	resp := req.do()?
+	if resp.status_code == 429 {
+		retry_after := resp.lheaders['retry-after'].int()
+		if 'x-ratelimit-global' in resp.lheaders && resp.lheaders['x-ratelimit-global'] == 'true' {
+			util.log('rate limit: global, retrying in $retry_after seconds.')
+			rest.rl.global_lock.m_lock()
+			time.sleep(retry_after)
+			rest.rl.global_lock.unlock()
+		} else {
+			util.log('rate limit: $bucket, retrying in $retry_after seconds.')
+			time.sleep(retry_after)
+		}
+		mlock.unlock()
+		return rest.do(req)
+	} else {
+		if resp.lheaders['x-ratelimit-remaining'] == '0' {
+			reset_after := int(resp.lheaders['x-ratelimit-reset-after'].f64() + 1)
+			util.log('exhausted bucket: $bucket, will reset in $reset_after seconds.')
+			go fn (mut m sync.Mutex, s int) {
+				time.sleep(s)
+				m.unlock()
+			}(mut mlock, reset_after)
+		} else {
+			mlock.unlock()
+		}
+	}
 	return resp
 }
