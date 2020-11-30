0dbc348a4a16f7a028499724ae791e5384ec1b99
Anton Terisback <terisbackno@gmail.com>
2020-11-30 06:03:28 +0000

    🎢 Borrowed impl of ratelimits from the discordgo

--- rest/ratelimit.v
+++ rest/ratelimit.v
@@ -2,39 +2,121 @@ module rest
 
 import sync
 import time
+import x.json2 as json
+import discordv.util.modum
 
-struct RateLimit {
+struct RateLimiter {
 pub mut:
-	rw &sync.RwMutex = sync.new_rwmutex() // TODO: remove when maps are thread-safe
-	buckets map[string]&sync.Mutex = map[string]&sync.Mutex{}
-	global_lock &sync.Mutex = sync.new_mutex()
+	mutex &sync.Mutex = sync.new_mutex()
+	global modum.Modum
+	buckets map[string]&Bucket = map[string]&Bucket{}
 }
 
-fn (mut rl RateLimit) m_lock(bucket string) &sync.Mutex {
-	// TODO: uncomment when pr merged vlang/v#7003
-	//if !rl.global_lock.try_lock() {
-		rl.global_lock.m_lock()
-	//}
-	rl.global_lock.unlock()
-	rl.rw.r_lock()
-	if bucket !in rl.buckets {
-		rl.rw.r_unlock()
-		mut mlock := sync.new_mutex()
-		rl.rw.w_lock()
-		rl.buckets[bucket] = mlock
-		mlock.m_lock()
-		rl.rw.w_unlock()
-		return mlock
-	} else {
-		mut mlock := rl.buckets[bucket]
-		rl.rw.r_unlock()
-		mlock.m_lock()
-		return mlock
+pub fn new_ratelimiter() &RateLimiter {
+	return &RateLimiter{}
+}
+
+pub fn (mut rl RateLimiter) get_bucket(key string) &Bucket {
+	rl.mutex.m_lock()
+	defer {rl.mutex.unlock()}
+
+	if key in rl.buckets {
+		return rl.buckets[key]
+	}
+
+	bucket := &Bucket{
+		rl: rl
+		remaining: 1
+	}
+
+	rl.buckets[key] = bucket
+	return bucket
+}
+
+pub fn (mut rl RateLimiter) get_wait_time(bucket &Bucket, min_remaining int) modum.Modum {
+	now := modum.now()
+	if bucket.remaining < min_remaining && bucket.reset.after(now){
+		return bucket.reset.sub(now)
+	}
+
+	if now.before(rl.global) {
+		return rl.global.sub(now)
+	}
+
+	return 0
+}
+
+pub fn (mut rl RateLimiter) lock_bucket(key string) &Bucket {
+	mut bucket := rl.get_bucket(key)
+	return rl.lock_bucket_obj(mut bucket)
+}
+
+pub fn (mut rl RateLimiter) lock_bucket_obj(mut bucket &Bucket) &Bucket{
+	bucket.mutex.m_lock()
+
+	wait := rl.get_wait_time(bucket, 1)
+	if wait > 0 {
+		time.sleep_ms(wait)
+	}
+
+	bucket.remaining--
+	return bucket
+}
+
+struct Bucket {
+pub mut:
+	rl &RateLimiter
+	mutex &sync.Mutex = sync.new_mutex()
+	remaining int
+	limit int
+	reset modum.Modum
+}
+
+pub fn (mut bucket Bucket) release(headers map[string]string) {
+	defer {bucket.mutex.unlock()}
+
+	if headers.len == 0 {
+		return 
+	}
+
+	if 'retry-after' in headers {
+		retry_after := headers['retry-after'].i64()
+		reset_at := modum.now().add(retry_after)
+		if 'x-ratelimit-global' in headers {
+			bucket.rl.global = headers['x-ratelimit-global'].i64()
+		} else {
+			bucket.reset = reset_at
+		}
+	} else if 'x-ratelimit-reset' in headers && 'date' in headers {
+		date := time.parse_iso8601(headers['date']) or {
+			return 
+		}
+		discord_time := modum.from_v(date)
+		unix := modum.Modum(headers['x-ratelimit-reset'].i64() * 1000)
+		delta := unix.sub(discord_time) + 250 // 250 is a magic number
+		bucket.reset = modum.now().add(delta)
+	}
+
+	if 'x-ratelimit-remaining' in headers {
+		bucket.remaining = headers['x-ratelimit-remaining'].int()
 	}
 }
 
-fn (mut rl RateLimit) unlock(bucket string) {
-	rl.rw.r_lock()
-	rl.buckets[bucket].unlock()
-	rl.rw.r_unlock()
+struct TooManyRequests {
+pub mut:
+	message string
+	retry_after f32
+	global bool
+}
+
+pub fn (mut tmr TooManyRequests) from_json(f json.Any){
+	mut obj := f.as_map()
+	for k, v in obj{
+		match k {
+			'message' {tmr.message = v.str()}
+			'retry_after' {tmr.retry_after = v.f32()}
+			'global' {tmr.global = v.bool()}
+			else {}
+		}
+	}
 }
--- rest/rest.v
+++ rest/rest.v
@@ -4,49 +4,36 @@ import discordv.util
 import net.http
 import sync
 import time
+import x.json2 as json
 
 pub struct REST {
 pub:
 	token string
 mut:
-	rl &RateLimit
+	rl &RateLimiter
 }
 
 pub fn new(token string) &REST{
 	return &REST{
 		token: token
-		rl: &RateLimit{}
+		rl: &RateLimiter{}
 	}
 }
 
 pub fn (mut rest REST) do(req http.Request) ?http.Response {
-	bucket := req.url
-	mut mlock := rest.rl.m_lock(bucket)
-	resp := req.do()?
+	key := req.url
+	mut bucket := rest.rl.lock_bucket(key)
+	resp := req.do() or {
+		bucket.release(map[string]&Bucket{})
+		return error(err)
+	}
+	bucket.release(resp.lheaders)
 	if resp.status_code == 429 {
-		retry_after := resp.lheaders['retry-after'].int()
-		if 'x-ratelimit-global' in resp.lheaders && resp.lheaders['x-ratelimit-global'] == 'true' {
-			util.log('rate limit: global, retrying in $retry_after seconds.')
-			rest.rl.global_lock.m_lock()
-			time.sleep(retry_after)
-			rest.rl.global_lock.unlock()
-		} else {
-			util.log('rate limit: $bucket, retrying in $retry_after seconds.')
-			time.sleep(retry_after)
-		}
-		mlock.unlock()
+		mut obj := json.raw_decode(resp.text)?
+		mut tmr := TooManyRequests{}
+		tmr.from_json(obj)
+		time.sleep_ms(int(tmr.retry_after))
 		return rest.do(req)
-	} else {
-		if resp.lheaders['x-ratelimit-remaining'] == '0' {
-			reset_after := int(resp.lheaders['x-ratelimit-reset-after'].f64() + 1)
-			util.log('exhausted bucket: $bucket, will reset in $reset_after seconds.')
-			go fn (mut m sync.Mutex, s int) {
-				time.sleep(s)
-				m.unlock()
-			}(mut mlock, reset_after)
-		} else {
-			mlock.unlock()
-		}
 	}
 	return resp
 }
