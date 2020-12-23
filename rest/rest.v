53e6306ef178575276f18a6834d7179d282c4110
Anton Terisback <terisbackno@gmail.com>
2020-12-23 00:32:25 +0000

    🧦 Move REST errors into REST module

--- rest/rest.v
+++ rest/rest.v
@@ -37,3 +37,25 @@ pub fn (mut rest REST) do(req http.Request) ?http.Response {
 	}
 	return resp
 }
+
+// REST API Error Message
+struct ApiErrorMessage {
+	code int
+	errors json.Any
+	message string
+}
+
+// Rest Api Response Codes
+enum ResponseCode {
+	ok = 200
+	created = 201
+	no_content = 204
+	not_modified = 304
+	bad_request = 400
+	unauthorized = 401
+	forbidden = 403
+	not_found = 404
+	method_not_allowed = 405
+	too_many_requests = 429
+	gateway_unavailable = 502
+}
