dde52450f746ced5e3682eed37e2c20bd389d542
Surman <gregerrelis@gmail.com>
2021-02-24 11:28:51 +0100

    Fix function rename
    Rename builder.write to builder.write_string

--- rest/formdata/formdata.v
+++ rest/formdata/formdata.v
@@ -82,24 +82,24 @@ pub fn (f FormData) encode() string {
 	mut builder := strings.new_builder(200)
 	builder.write_b(`\n`)
 	for k, v in f.fields {
-		builder.write('--${f.boundary}\n')
+		builder.write_string('--${f.boundary}\n')
 		match v {
 			string {
-				builder.write('Content-Disposition: form-data; name=\"${k}\"\n')
+				builder.write_string('Content-Disposition: form-data; name=\"${k}\"\n')
 				builder.write_b(`\n`)
-				builder.write(v)
+				builder.write_string(v)
 				builder.write_b(`\n`)
 			}
 			FormFile {
-				builder.write('Content-Disposition: form-data; name=\"${k}\"; filename=\"${v.filename}\"\n')
-				builder.write('Content-Type: ${v.content_type}\n')
-				builder.write('Content-Transfer-Encoding: base64\n')
+				builder.write_string('Content-Disposition: form-data; name=\"${k}\"; filename=\"${v.filename}\"\n')
+				builder.write_string('Content-Type: ${v.content_type}\n')
+				builder.write_string('Content-Transfer-Encoding: base64\n')
 				builder.write_b(`\n`)
-				builder.write(base64.encode(v.data.bytestr()))
+				builder.write_string(base64.encode(v.data.bytestr()))
 				builder.write_b(`\n`)
 			}
 		}
 	}
-	builder.write('--${f.boundary}--')
+	builder.write_string('--${f.boundary}--')
 	return builder.str()
 }
