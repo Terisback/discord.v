baca984cc9bcb70432129edc705bfa777e0fb1b9
Saruman The Dead <70163032+SurmanPP@users.noreply.github.com>
2021-02-25 13:17:51 +0100

    Remove deprecated function calls (#15)
    * Remove deprecated function calls
    
    time.sleep_ms is deprecated replace with wait
    
    * Change conversion of time
    
    Use functions instead of number for time conversion
    
    * Change time conversion
    
    Add multiplier for duartions

--- rest/ratelimit.v
+++ rest/ratelimit.v
@@ -56,7 +56,7 @@ pub fn (mut rl RateLimiter) lock_bucket_obj(mut bucket Bucket) &Bucket {
 	bucket.mutex.@lock()
 	wait := rl.get_wait_time(bucket, 1)
 	if wait > 0 {
-		time.sleep_ms(int(wait))
+		time.wait(time.milliseconds * wait)
 	}
 	bucket.remaining--
 	return bucket
